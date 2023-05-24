f044b9a5ea30e07db3245d4fd7c86c4d401d0b0a
IT22604958 <127285446+IT22604958@users.noreply.github.com>
2023-05-24 10:54:59 +0530

    Added faq

---
+++ faq.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatable" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/faq.css">
+    <title>Socail Ad Wizards</title>
+</head>
+
+<body>
+    <!-- open Navigation bar with PHP -->
+    <?php include 'header.php' ?>
+
+<div class=controler">
+
+<h1>test</h1></br>
+<h1>test</h1></br>
+    <h1>FAQ</h1>
+	
+<form method="POST" action="#">
+Question :
+<input type ="text" placeholder="  Ask your Question...." size="1000">
+<br>
+<br>
+
+<button class="button">Submit</button>
+
+</form>
+</div>
+    <!-- Footer bar with PHP -->
+    <?php include 'footer.php' ?>
+</body>
+</html>
