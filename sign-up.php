870cd03ffe0d928c3799d77ee5f62fc9acc1deb2
navin <it22361240@my.sliit.lk>
2023-06-02 14:46:32 +0530

    css with sign up
    If there are more things to add

--- sign-up.php
+++ sign-up.php
@@ -9,159 +9,133 @@ if (isset($_SESSION['email'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang = "en">
-  <!--
-  <style>
-    body{
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      font-family: 'poppins',sans-serif;
-      background-color:rgb(158, 213, 197);
-      background-position: center;
-    }
-    header{
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      padding: 20px 100px;
-      background-color:rgb(142, 195, 176);
-      display: flex;
-      justify-content: space-evenly;
-      align-items: center;
-      z-index: 80;
-    }
-    .logo{
-      font-size: 3em;
-      color:rgb(145, 127, 179);
-      user-select: none;
-    }
-    .navigation a {
-      position: relative;
-      font-size: 1.5em;
-      color:rgb(44, 44, 139);
-      text-decoration: none;
-      font-weight: 500;
-      margin-left: 40px ;
-    }
-    .navigation .btnlogin-popup{
-      width: 130px;
-      height: 50px;
-      background: transparent;
-      border: 2px solid rgb(10, 10, 52);
-      outline: none;
-      border-radius: 6px;
-      cursor: pointer;
-      font-size: 1.1em;
-      color: rgb(20, 49, 74);
-      font-weight: 500;
-      margin-left: 40px;
-    }
-    .signin{
-      width: 400px; 
-    height: 500px;
-    border: 2px solid rgb(134, 134, 224);  
-    border-radius: 20px;
-    backdrop-filter: blur(20px);
-    background-color:rgb(188, 234, 213);
-    position: relative;
-    box-shadow: 0 0 30px rgb(142, 195, 176); 
-    top:200px; 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    bottom: 0; 
-    left: 0; 
-    right: 0; 
- 
-    margin: auto;
-    }
-    .button {
-      width: 100%;
-      height: 45px;
-      background:rgb(148, 175, 159);
-      border: none;
-      outline: none;
-      border-radius: 9px;
-      cursor: pointer;
-      font-size: 1em;
-      font-weight: 500;
-    }
-    .signin_google{
-      width: 100%;
-      height: 45px;
-      background:rgb(148, 175, 159);
-      border: none;
-      outline: none;
-      border-radius: 9px;
-      cursor: pointer;
-      font-size: 1em;
-      font-weight: 500;
+<html lang="en">
 
-    }
-    .form_box h3 {
-      font-size: 2em;
-      color: rgb(23, 23, 88);
-      text-align: center;
-    }
-    .input_field{
-      width: 100%;
-      height: 30px;
-    }
-    .agree{
-      font-size: .9em;
-      font-weight: 500;
-      margin: -15px 0 15px;
-      display: flex;
-      justify-content: space-between;
-      color: rgb(35, 35, 35);
-
-    }
-
-
-    </style>-->
 <head>
-<title>Sign Up</title>
+  <title>Sign up</title>
+  <style>
+    * {
+      box-sizing: border-box;
+    }
+
+    body {
+      background-color: #1d7ee5;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      font-family: Arial, sans-serif;
+    }
+
+    .form {
+      width: 500px;
+      background-color: rgba(255, 255, 255, 0.13);
+      border-radius: 10px;
+      padding: 50px;
+      box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
+      backdrop-filter: blur(10px);
+    }
+
+    .form h3 {
+      text-align: center;
+      font-size: 24px;
+      color: #fff;
+      margin-bottom: 30px;
+    }
+
+    .form-row {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: 20px;
+      margin-bottom: 20px;
+    }
+
+    .form-row label {
+      font-size: 16px;
+      font-weight: bold;
+      color: #fff;
+      margin-bottom: 5px;
+    }
+
+    .form-row input,
+    .form-row select {
+      width: 100%;
+      height: 40px;
+      padding: 5px 10px;
+      border-radius: 3px;
+      border: none;
+      background-color: rgba(255, 255, 255, 0.07);
+      color: #fff;
+      font-size: 14px;
+    }
+
+    .form-row .input_field {
+      position: relative;
+    }
+
+    .form-row .input_field span {
+      position: absolute;
+      top: 50%;
+      left: 15px;
+      transform: translateY(-50%);
+      color: #fff;
+    }
+
+    .form-row .input_field input {
+      padding-left: 40px;
+    }
+
+    .form-row .input_field i {
+      margin-right: 4px;
+    }
+
+    .form-row .agree label {
+      font-size: 14px;
+    }
+
+    .submit-btn {
+      display: flex;
+      justify-content: center;
+    }
+
+    .button {
+      margin-top: 50px;
+    width: 100%;
+    background-color: #ffffff;
+    padding: 15px 0;
+    font-size: 18px;
+    font-weight: 600;
+    border-radius: 1px;
+    cursor: pointer;
+    border-radius: 8px;
+    border: 0;
+    }
+    .agree {
+    display: flex;
+    align-items: flex-start;
+    size:5px ;
+    color: #fff;
+    }
+  </style>
 </head>
 
-<!-- Please remove this header and header css
 <body>
-<header>
-  <h1 class ="logo" >Ad Wizard logo </h1>
-  <nav class="navigation">
-    <a href="#">Home</a>
-    <a href="#">Gallery</a>
-    <a href="#">Services</a>
-    <a href="#">About</a>
-    <a href="#">Contact</a>
-    <button class="btnlogin-popup">Login</button>
-  </nav>
-</header>
-  -->
-
-
-<div class="signin">
-  <div class="form_box">
-    <h3> Sign in</h3>
+  <div class="form">
     <form action="action.php" method="POST">
-
-        <div class="row clearfix">
-          <div class="col_half">
-            <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i><ion-icon name="person-add"></ion-icon></span>
-              <label for="fname">First Name</label><br>
-              <input type="text" name="fname" placeholder="First Name">
-            </div>
-          </div>
-          <div class="col_half">
-            <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i><ion-icon name="person-add"></ion-icon></span>
-            <label for="lname">Last Name</label><br>
-            <input type="text" name="lname" placeholder="Last Name" required>
-            </div>
-          </div>
+      <h3>Sign up</h3>
+      <div class="form-row">
+        <div class="input_field">
+          <span><i class="fa fa-user" aria-hidden="true"></i></span>
+          <label for="fname">First Name</label>
+          <input type="text" name="fname" placeholder="First Name" required>
         </div>
-  
-        <div class="input_field"> <span><i aria-hidden="true" class="fa fa-envelope"></i><ion-icon name="mail"></ion-icon></span>
+        <div class="input_field">
+          <span><i class="fa fa-user" aria-hidden="true"></i></span>
+          <label for="lname">Last Name</label>
+          <input type="text" name="lname" placeholder="Last Name" required>
+          
+        </div>
+        <div class="input_field"> <span><i aria-hidden="true" class="fa fa-envelope"></i></span>
           <label for="email">E-mail</label><br>
           <input type="email" name="email" placeholder="E-mail" required>
         </div>
@@ -193,27 +167,18 @@ if (isset($_SESSION['email'])) {
           <label for="password">Confirm Password</label><br>
           <input type="password" name="password" placeholder="Re-type Password" required>
         </div>
-
-            <input type="text" name="role"><br>
-    
-              <div class="select_arrow"></div>
-            </div>
-          <div class="agree">
-            <input type="checkbox" id="cb1">
-        <label for="cb1">I accept the Terms of use & Privacy policy</label>
-          </div>
-          <div class="input_field checkbox_option"></div>
-        <input class="button" type="submit" name="signup" value="Sign Up"/>
-      </div>
-</div>
-
-
-    </form>
-  </div>
-</div>
-
-<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
- <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
   
+         
+      </div>
+      <div class="agree">
+        <input type="checkbox" id="cb1">
+    <label for="cb1">I accept the Terms of use & Privacy policy</label>
+      </div>
+      <div class="input_field checkbox_option"></div>
+    <input class="button" type="submit" name="signup" value="Sign Up"/>
+    </div>
+  </form>
 </body>
-</html> 
+</html>
+
+      
