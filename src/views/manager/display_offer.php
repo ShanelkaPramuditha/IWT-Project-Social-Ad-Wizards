e4b2db57d714d8eca9948755b1437e293f7c3ae4
Shanelka Pramuditha <99pramuditha@gmail.com>
2023-06-06 22:10:28 +0530

    error fixed

--- src/views/manager/display_offer.php
+++ src/views/manager/display_offer.php
@@ -1,20 +1,20 @@
-<?
- require"config.php";
- $sql="select*from my Table";
- $result = $con->query($sql);
-      if($result->num_rows>0)
-	  {
-		  $row =$result->fetch_assoc())
-	  echo"Full name:".$row["full name"];
-	  echo"E-Mail:".$row["E-Mail"];
-	  echo"Address:".$row["Address"];
-	  echo"Phone number:".$row["Phone number"];
-	  echo"provinces:".$row["Province"];
-	  echo"offers:".$row["Offers"];
-	  }
-	  else
-	  {
-		  echo " not correct";
-	  }
-	  $con->close();
-	  ?>
+<?php
+require "config.php";
+
+$sql = "SELECT * FROM myTable";
+$result = $con->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    echo "Full name: " . $row["full_name"] . "<br>";
+    echo "E-Mail: " . $row["E-Mail"] . "<br>";
+    echo "Address: " . $row["Address"] . "<br>";
+    echo "Phone number: " . $row["Phone_number"] . "<br>";
+    echo "Provinces: " . $row["Province"] . "<br>";
+    echo "Offers: " . $row["Offers"] . "<br>";
+} else {
+    echo "No records found";
+}
+
+$con->close();
+?>
--- src/views/manager/offer_ insert.php
+++ src/views/manager/offer_ insert.php
@@ -1,20 +1,20 @@
 <?php
-if( insset($_POST["Submit"]))
-  {$name=$_POST["Full name"];
-  {$email=$_POST["Email"];
-  {$Address=$_POST["Address"];
-  {$phone number=$_POST["phone nuumber"];
-  {$provinces=$_POST["provinces"];
-  {$Offers=$_POST["Offers"];
-  $sql="INSERT INTO Offers_details (owner_Full name,owner_Email,owner_Address,owner_Phone number,provinces,offers)VALUES($name,$email,$Address,$phonenumber,$provinces,$Offers)";
-  
-     if($con->query($sql)){
-		 echo" Successfully";
-	 }
-	 else{
-		 echo"Error! Try again."$con->error;
-	 }
-	 $con->close();
-	 
-  
- ?> 
+if (isset($_POST["Submit"])) {
+    $name = $_POST["Full name"];
+    $email = $_POST["Email"];
+    $address = $_POST["Address"];
+    $phoneNumber = $_POST["phone number"];
+    $provinces = $_POST["provinces"];
+    $offers = $_POST["Offers"];
+
+    $sql = "INSERT INTO Offers_details (owner_Full_name, owner_Email, owner_Address, owner_Phone_number, provinces, offers) VALUES ('$name', '$email', '$address', '$phoneNumber', '$provinces', '$offers')";
+
+    if ($con->query($sql)) {
+        echo "Successfully inserted record.";
+    } else {
+        echo "Error! Try again: " . $con->error;
+    }
+
+    $con->close();
+}
+?>
