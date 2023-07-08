a64922f18cc4a85422c4a470a2d37d34e435ecd2
Shanelka Pramuditha <74038575+ShanelkaPramuditha@users.noreply.github.com>
2023-07-08 22:55:30 +0530

    Update SQLQuery-final.sql

--- SQLQuery-final.sql
+++ SQLQuery-final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2023 at 03:53 AM
+-- Generation Time: Jul 08, 2023 at 07:24 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -77,7 +77,9 @@ INSERT INTO `faq` (`faq_id`, `s_user_id`, `question`, `answer`) VALUES
 (17, 4, 'How can I pay for the product', 'You can pay with card'),
 (100, 2, 'How much time is needed for ad design?', 'At least 3 days'),
 (111, 2, 'Why should I choose Social Ad Wizard', 'Because Social Ad Wizard is a very trustfull and very user friendly compamy. So Customers can satisfied about the final product definitly'),
-(112, 2, 'Can I make changes to an order I already placed', 'No. You cannot make changes After place a order');
+(112, 2, 'Can I make changes to an order I already placed', 'No. You cannot make changes After place a order'),
+(114, 34, 'test test test', NULL),
+(115, 38, 'how can I do the proper advertise', 'yes');
 
 -- --------------------------------------------------------
 
@@ -98,7 +100,7 @@ CREATE TABLE `gallery` (
 --
 
 INSERT INTO `gallery` (`g_id`, `g_title`, `g_link`, `g_review`, `s_user_id`) VALUES
-(29, 'Sample 1', './assets/images/uploads/gallery/649898734f807_Black White Elegant Hijab Fashion Product Sale Facebook Post.png', 0, NULL),
+(29, 'Sample 1', './assets/images/uploads/gallery/649936c7e4f0a_Black White Elegant Hijab Fashion Product Sale Facebook Post.png', 0, NULL),
 (30, 'Sample 2', './assets/images/uploads/gallery/64989888d24a3_White Simple Creative Quote Facebook Post.png', 0, NULL),
 (31, 'Sample 3', './assets/images/uploads/gallery/649898959ea7c_Blue White Simple Special Offer Facebook Post.png', 0, NULL),
 (32, 'Sample 4', './assets/images/uploads/gallery/6498989f75142_Creative Blue Fashion Special Offer (Facebook Post).png', 0, NULL),
@@ -134,7 +136,8 @@ CREATE TABLE `offer` (
 --
 
 INSERT INTO `offer` (`offer_id`, `offer_percentage`, `o_start_date`, `o_end_date`) VALUES
-(24, 20.00, '2023-06-26', '0000-00-00');
+(32, 10.00, '2023-06-26', '0000-00-00'),
+(33, 25.00, '2023-06-26', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -157,6 +160,13 @@ CREATE TABLE `order_info` (
   `released_link` varchar(500) DEFAULT NULL,
   `released_date` date DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_info`
+--
+
+INSERT INTO `order_info` (`order_id`, `s_user_id`, `order_date`, `order_status`, `ad_platform`, `order_desc`, `category`, `ad_format`, `documents`, `offer_id`, `order_value`, `released_link`, `released_date`) VALUES
+(67, 38, '2023-06-26', 'Completed', 'Facebook', 'I want the create profile picture. 200px*400px', 'Electric Item', 'pictu', NULL, NULL, 2400, 'Product', '2023-06-26');
 
 -- --------------------------------------------------------
 
@@ -186,7 +196,8 @@ INSERT INTO `registered_users` (`s_user_id`, `registered_date`, `first_name`, `l
 (1, '2023-06-11', 'Luvi', 'Dias', 'admin@mail.com', '0773456789', '1994-09-01', 'female', 'assets/images/uploads/profile-pictures/default.jpg', '$2y$10$AGHT/JhfvLw5Bwtg8TzF8.YxD0uwepGj9bfUOHOB4IZj8m5r27mei', 'admin'),
 (2, '2023-06-11', 'Olivia', 'Carter', 'manager@mail.com', '0783456789', '1996-09-01', 'male', 'assets/images/uploads/profile-pictures/default.jpg', '$2y$10$aQiIRRQyFIi/TYJcteQHwOMjUHXpzHAL1.buEBqMBVQgYDmxPT/H.', 'manager'),
 (3, '2023-06-11', 'Don', 'Brook', 'designer@mail.com', '0713456789', '1998-09-01', 'male', 'assets/images/uploads/profile-pictures/default.jpg', '$2y$10$VicNJHMgAzVtwOEmwa04LuREYIaBerlEtPG8IAR7grFWjqY8kc33e', 'designer'),
-(34, '2023-06-26', 'Don', 'David', 'user@mail.com', '0771234567', '2000-04-26', 'male', 'assets/images/uploads/profile-pictures/user1.jpg', '$2y$10$GSNGnEWJE6h9dSWpezY5P.NRqiI39fn44Bzbo8M1EHAaK8aDA4MQS', 'user');
+(36, '2023-06-26', 'Shanelka', 'Shan', 'sss@mail.com', '1234567890', '2010-06-02', 'male', 'assets/images/uploads/profile-pictures/default.jpg', '$2y$10$AhpC/oU6dttLkiVNcfybX.QReUd8Fo00hSkwPk3gyAcf2TovIEUa6', 'manager'),
+(38, '2023-06-26', 'Don', 'Dddd', 'user@mail.com', '0123456789', '2006-06-28', 'male', 'assets/images/uploads/profile-pictures/default.jpg', '$2y$10$pW87wXEHmWiYcedh3JBGa.pR5YwQRhNkwwNzs51Gzv0pnH2wcSxN2', 'user');
 
 --
 -- Indexes for dumped tables
@@ -245,31 +256,31 @@ ALTER TABLE `contact_messages`
 -- AUTO_INCREMENT for table `faq`
 --
 ALTER TABLE `faq`
-  MODIFY `faq_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
+  MODIFY `faq_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
 
 --
 -- AUTO_INCREMENT for table `gallery`
 --
 ALTER TABLE `gallery`
-  MODIFY `g_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `g_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `offer`
 --
 ALTER TABLE `offer`
-  MODIFY `offer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `offer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `order_info`
 --
 ALTER TABLE `order_info`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- AUTO_INCREMENT for table `registered_users`
 --
 ALTER TABLE `registered_users`
-  MODIFY `s_user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `s_user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- Constraints for dumped tables
